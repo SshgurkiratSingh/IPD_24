c58ef0ffef58a29defca02fd14c5013966b133bd
Gurkirat Singh <63462904+SshgurkiratSingh@users.noreply.github.com>
2024-11-11 19:53:42 +0530

    Automated Push

--- frontEnd/app/page.tsx
+++ frontEnd/app/page.tsx
@@ -8,11 +8,19 @@ import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 import ChatPage from "./chatPage";
 import "regenerator-runtime/runtime"; // Add this line
+import { Card, CardFooter } from "@nextui-org/card";
+import { Divider } from "@nextui-org/divider";
 
 export default function Home() {
   return (
     <section className="flex flex-row items-center justify-center min-w-full min-h-full ">
-      <ChatPage />
+      <Card>
+        <ChatPage />
+        <Divider className="neon-divider" />
+        <CardFooter>
+          
+        </CardFooter>
+      </Card>
     </section>
   );
 }
