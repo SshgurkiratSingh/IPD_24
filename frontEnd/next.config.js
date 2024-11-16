500f107671be81f96d49fd16db1ceb7b2d07599e
Gurkirat Singh <63462904+SshgurkiratSingh@users.noreply.github.com>
2024-11-16 23:02:35 +0530

    Automated Push

--- frontEnd/next.config.js
+++ frontEnd/next.config.js
@@ -6,7 +6,8 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:2500/api/:path*",
+        destination:
+          "http://ec2-3-86-53-202.compute-1.amazonaws.com:2500/api/:path*",
       },
     ];
   },
